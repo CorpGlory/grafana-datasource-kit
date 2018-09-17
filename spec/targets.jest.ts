63039e6491753e097a53f4f320cd0dc5767d56d7
Evgeny Smyshlyaev <amper43@corpglory.com>
2018-09-17 19:35:52 +0300

    add simple tests (#13)

--- spec/targets.jest.ts
+++ spec/targets.jest.ts
@@ -6,13 +6,39 @@ describe('Correct InfluxDB query', function() {
     type: 'influxdb',
     params: {
       db: 'db',
-      q: 'q',
-      epoch: '0-1'
+      q: '',
+      epoch: ''
     }
   }
-  let query = new GrafanaMetric(datasource, [1,2])
+  let target = 'mean("value")'
+  let query = new GrafanaMetric(datasource, [target])
 
   it("test", function() {
-      expect(query.metricQuery.getQuery(0,1,2,3)).toBe('check')
+      expect(query.metricQuery.getQuery(1534809600,1537488000,100,20)).toBe(
+        `SELECT mean("value") FROM "db" WHERE time > '2018-08-21T00:00:00' and time <= '2018-09-21T00:00:00' \\
+          LIMIT 100 OFFSET 20`
+        )
+  })
+})
+
+describe('correct Graphite query', function() {
+  let datasource: GrafanaDatasource = {
+    url: 'http://example.com:1234',
+    type: 'graphite',
+    params: {
+      db: '',
+      q: '',
+      epoch: ''
+    }
+  }
+
+  let target = `target=template(hosts.$hostname.cpu, hostname="worker1")`
+  let query = new GrafanaMetric(datasource, [target])
+
+  it("test simple query with time clause", function () {
+    expect(query.metricQuery.getQuery(1534809600, 1537488000, 0, 0)).toBe(
+      `http://example.com:1234/render?target=template(hosts.$hostname.cpu, hostname="worker1") \\
+        &from=00:00_20180821&until=00:00_20180921'`
+      )
   })
 })
