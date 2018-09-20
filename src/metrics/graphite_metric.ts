c0a577e251ed3ea06d7962cbf51222361da71ae5
amper43 <amper43@corpglory.com>
2018-09-20 06:17:43 +0300

    fix time clause

--- src/metrics/graphite_metric.ts
+++ src/metrics/graphite_metric.ts
@@ -1,5 +1,7 @@
 import { AbsractMetric, Datasource, MetricId  } from './metric';
 
+import * as moment from 'moment';
+
 export class GraphiteMetric extends AbsractMetric {
 
     private static QUERY_TIME_REGEX = /\&from=[^\&]+\&until=[^\&]+\&/;
@@ -16,11 +18,12 @@ export class GraphiteMetric extends AbsractMetric {
 
     getQuery(from: number, to: number, limit: number, offset: number): string {
 
-      //move to seconds
-      from = from / 1000;
-      to = to / 1000;
+      let moment_format = 'h:mm:ss_YYYYMMDD';
+      let from_date = moment(from).format(moment_format);
+      let to_date = moment(to).format(moment_format);
 
-      let timeClause = `&from=${from}s&until=${to}s`;
-      return `${this._queryParts[0]}${timeClause}&${this._queryParts[1]}&maxDataPoints=${limit}`;
+
+      let timeClause = `&from=${from_date}&until=${to_date}`;
+      return `/?${this._queryParts[0]}${timeClause}&${this._queryParts[1]}&maxDataPoints=${limit}`;
     }
 }
