7f8be94459e21707bee789cc195d7f38260a5d32
amper43 <amper43@corpglory.com>
2018-09-20 14:44:51 +0300

    fix according review

--- spec/targets.jest.ts
+++ spec/targets.jest.ts
@@ -18,7 +18,7 @@ describe('Correct InfluxDB query', function() {
       expect(query.metricQuery.getQuery(1534809600,1537488000,100,20)).toBe(
         `SELECT mean("value") FROM "db" WHERE time > '2018-08-21T00:00:00' and time <= '2018-09-21T00:00:00' \\
           LIMIT 100 OFFSET 20`
-        )
+      )
   })
 })
 
@@ -39,6 +39,6 @@ describe('correct Graphite query', function() {
   it("test simple query with time clause", function () {
     expect(query.metricQuery.getQuery(1534809600, 1537488000, 500, 0)).toBe(
       `?target=template(hosts.$hostname.cpu, hostname="worker1")&from=00:00_20180821&until=00:00_20180921&maxDataPoints=500`
-      )
+    )
   })
 })
