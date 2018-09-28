46b02189d15061e824390edcd48f6460946157a2
Evgeny Smyshlyaev <amper43@corpglory.com>
2018-09-28 17:30:25 +0300

    Prometheus one metric instead of multiple #26 (#29)
    return matrix for each lables set

--- src/metrics/prometheus_metric.ts
+++ src/metrics/prometheus_metric.ts
@@ -17,7 +17,8 @@ export class PrometheusMetric extends AbstractMetric {
   }
 
   getResults(res) {
-    if(res.data === undefined || res.data.result.length < 1) {
+    
+    if(res.data === undefined || res.data.data.result.length < 1) {
       console.log('datasource return empty response, no data');
       return {
         columns: ['timestamp', 'target'],
@@ -25,14 +26,49 @@ export class PrometheusMetric extends AbstractMetric {
       };
     }
 
-    let metric_name = '';
-    for(let key in res.data.data.result[0].metric) {
-      metric_name = `${metric_name ? `${metric_name}:` : '' }${key}=${res.data.data.result[0].metric[key]}`;
-    }
-
-    return {
-      columns: ['timestamp', metric_name],
-      values: res.data.data.result[0].values
+    let result = res.data.data.result;
+    let result_matrix = {
+      columns: ['timestamp'],
+      values: []
     };
+
+    result.map(r => {
+      let keys = [];
+      for(let key in r.metric) {
+        keys.push(`${key}=${r.metric[key]}`);
+      }
+      result_matrix.columns.push(keys.join(':'));
+    });
+
+    let values = result.map(r => r.values);
+
+    let timestamps = [];
+    values.map(v => v.map(row => timestamps.push(row[0])));
+    timestamps = timestamps.filter(function(item, i, ar) { 
+      return ar.indexOf(item) === i; //uniq values
+    });
+
+    for(let t of timestamps) {
+      let row = [t];
+      values.map(v => {
+        if(v[0] === undefined) {
+          row.push(0);
+        }
+
+        let currentTimestamp = v[0][0];
+        let currentValue = v[0][1];
+
+        if(currentTimestamp === t) {
+          row.push(currentValue);
+          v.shift();
+        }
+        else {
+          row.push('');
+        }
+      });
+      result_matrix.values.push(row);
+    };
+
+    return result_matrix;
   }
 }
