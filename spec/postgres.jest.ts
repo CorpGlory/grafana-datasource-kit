d3587aacbb244b4be8829505030cd9404b4a418b
amper43 <amper43@corpglory.com>
2018-12-01 21:25:57 +0300

    improve tests

--- spec/postgres.jest.ts
+++ spec/postgres.jest.ts
@@ -6,33 +6,12 @@ import * as _ from 'lodash';
 
 describe('Test query creation', function() {
 
-  let queryPayload = {
-    from: 1542983750857,
-    to: 1542984313292,
-    queries:[{
-      refId: 'A',
-      intervalMs:2000,
-      maxDataPoints:191,
-      datasourceId:1,
-      rawSql: 'SELECT\n  \"time\" AS \"time\",\n  val\nFROM local\nORDER BY 1',
-      format: 'time_series'
-    }]
-  };
-
-  let datasource = {
-      url: 'api/tsdb/query',
-      type: 'postgres',
-      data: queryPayload
-  };
-
-  let targets = [{
-    refId: 'A',
-  }];
-
   let limit = 1000;
   let offset = 0;
-  let postgres = new PostgresMetric(datasource, targets);
-  let mQuery: MetricQuery = postgres.getQuery(queryPayload.from, queryPayload.to, limit, offset);
+  let from = 1542983750857;
+  let to = 1542984313292;
+  let postgres = getDefaultMetric();
+  let mQuery: MetricQuery = postgres.getQuery(from, to, limit, offset);
 
   it('test that payload placed to data field', function() {
     expect('data' in mQuery.schema).toBeTruthy();
@@ -48,7 +27,10 @@ describe('Test query creation', function() {
   it('method should be POST', function() {
     expect(mQuery.method.toLocaleLowerCase()).toBe('post');
   });
+});
 
+describe('Test result parsing', function() {
+  let postgres = getDefaultMetric();
   let timestamps = [1542983800000, 1542983800060, 1542983800120]
   let response = {
     data: {
@@ -82,3 +64,147 @@ describe('Test query creation', function() {
     expect(result.values.map(v => v[timestampColumnNumber])).toEqual(timestamps);
   });
 });
+
+describe('Test sql processing', function() {
+  let limit = 1000;
+  let offset = 77;
+  let from = 1542983750857;
+  let to = 1542984313292;
+
+  it('simple sql with one select', function() {
+    let original = `SELECT
+    \"time\" AS \"time\",
+      val
+    FROM local
+    ORDER BY 1`;
+    let expected = `SELECT
+    \"time\" AS \"time\",
+      val
+    FROM local
+    ORDER BY 1 LIMIT ${limit} OFFSET ${offset}`;
+    let metric = getMetricWithSql(original);
+    expect(metric.getQuery(from, to, limit, offset).schema.data.queries[0].rawSql).toBe(expected);
+  });
+
+  it('sql with xxx offset limit', function() {
+    let original = `WITH RECURSIVE t(n) AS (
+      VALUES (1)
+      UNION ALL
+      SELECT n+1 FROM t WHERE n < 100
+     )
+     SELECT sum(n) FROM t;) OFFSET xxx LIMIT xxx;`;
+    let expected = `WITH RECURSIVE t(n) AS (
+      VALUES (1)
+      UNION ALL
+      SELECT n+1 FROM t WHERE n < 100
+      )
+     SELECT sum(n) FROM t;) OFFSET ${offset} LIMIT ${limit};`
+    let metric = getMetricWithSql(original);
+    expect(metric.getQuery(from, to, limit, offset).schema.data.queries[0].rawSql).toBe(expected);
+  });
+  
+  it('complex sql with one select', function() {
+    let original = `SELECT
+    statistics.created_at as time,
+   CAST(statistics.value AS decimal) as value,
+    sensor.title as metric
+   FROM statistics
+   INNER JOIN sensor
+   ON sensor.id = statistics.sensor_id
+   WHERE
+    statistics.device_id = '000-aaaa-bbbb'
+    AND sensor.type = 5
+    AND sensor.section_id IN($section_id)
+    AND statistics.value != 'ERR'
+    AND statistics.value !='???'
+    AND $__timeFilter(statistics.created_at)`;
+    let expected = `SELECT
+    statistics.created_at as time,
+   CAST(statistics.value AS decimal) as value,
+    sensor.title as metric
+   FROM statistics
+   INNER JOIN sensor
+   ON sensor.id = statistics.sensor_id
+   WHERE
+    statistics.device_id = '000-aaaa-bbbb'
+    AND sensor.type = 5
+    AND sensor.section_id IN($section_id)
+    AND statistics.value != 'ERR'
+    AND statistics.value !='???'
+    AND $__timeFilter(statistics.created_at) LIMIT ${limit} OFFSET ${offset}`;
+    let metric = getMetricWithSql(original);
+    expect(metric.getQuery(from, to, limit, offset).schema.data.queries[0].rawSql).toBe(expected);
+  })
+
+  it('sql with number of nested select', function() {
+    let original = `WITH regional_sales AS (
+      SELECT region, SUM(amount) AS total_sales
+      FROM orders
+      GROUP BY region LIMIT 5 OFFSET 1
+     ), top_regions AS (
+      SELECT region
+      FROM regional_sales
+      WHERE total_sales > (SELECT SUM(total_sales)/10 FROM regional_sales)
+      LIMIT 3
+     )
+     SELECT region,
+      product,
+      SUM(quantity) AS product_units,
+      SUM(amount) AS product_sales
+     FROM orders
+     WHERE region IN (SELECT region FROM top_regions)
+     GROUP BY region, product OFFSET 500;`;
+    let expected = `WITH regional_sales AS (
+      SELECT region, SUM(amount) AS total_sales
+      FROM orders
+      GROUP BY region LIMIT 5 OFFSET 1
+     ), top_regions AS (
+      SELECT region
+      FROM regional_sales
+      WHERE total_sales > (SELECT SUM(total_sales)/10 FROM regional_sales)
+      LIMIT 3
+     )
+     SELECT region,
+      product,
+      SUM(quantity) AS product_units,
+      SUM(amount) AS product_sales
+     FROM orders
+     WHERE region IN (SELECT region FROM top_regions)
+     GROUP BY region, product LIMIT ${limit} OFFSET ${offset};`;
+    let metric = getMetricWithSql(original);
+    expect(metric.getQuery(from, to, limit, offset).schema.data.queries[0].rawSql).toBe(expected);
+  });
+});
+
+function getMetricWithSql(sql: string): PostgresMetric {
+  let metric = getDefaultMetric();
+  metric.datasource.data.queries[0].rawSql = sql;
+  return metric;
+}
+
+function getDefaultMetric(): PostgresMetric {
+  let queryPayload = {
+    from: 1542983750857,
+    to: 1542984313292,
+    queries:[{
+      refId: 'A',
+      intervalMs:2000,
+      maxDataPoints:191,
+      datasourceId:1,
+      rawSql: 'SELECT\n  \"time\" AS \"time\",\n  val\nFROM local\nORDER BY 1',
+      format: 'time_series'
+    }]
+  };
+
+  let datasource = {
+      url: 'api/tsdb/query',
+      type: 'postgres',
+      data: queryPayload
+  };
+
+  let targets = [{
+    refId: 'A',
+  }];
+
+  return new PostgresMetric(datasource, targets);
+}
