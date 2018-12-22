4a2d1be5799214b273cf1b25210a99c47057cf84
Evgeny Smyshlyaev <amper43@gmail.com>
2018-12-22 11:07:44 +0300

    Correct processing of time clause and maxDataPoints params (#47)

--- src/metrics/graphite_metric.ts
+++ src/metrics/graphite_metric.ts
@@ -1,10 +1,7 @@
 import { AbstractMetric, Datasource, MetricId, MetricQuery, MetricResults  } from './metric';
 
+import * as _ from 'lodash';
 import * as moment from 'moment';
-
-
-const QUERY_TIME_REGEX = /\&from=[^\&]*\&until=[^\&]*\&/;
-const MAX_DATA_POINTS_REGEX = /\&maxDataPoints=[^\&]+/;
 
 
 export class GraphiteMetric extends AbstractMetric {
@@ -13,21 +10,36 @@ export class GraphiteMetric extends AbstractMetric {
 
   constructor(datasource: Datasource, targets: any[], id?: MetricId) {
     super(datasource, targets, id);
-    let queryStr = datasource.data;
-    this._queryParts = queryStr.split(QUERY_TIME_REGEX);
-    this._queryParts[1] = this._queryParts[1].split(MAX_DATA_POINTS_REGEX)[0];
   }
 
   getQuery(from: number, to: number, limit: number, offset: number): MetricQuery {
-
     let moment_format = 'h:mm_YYYYMMDD';
     let from_date = moment(from).format(moment_format);
     let to_date = moment(to).format(moment_format);
 
-    let timeClause = `&from=${from_date}&until=${to_date}`;
-    let q = `?${this._queryParts[0]}${timeClause}&${this._queryParts[1]}&maxDataPoints=${limit}`;
+    let fromRegex = /from=[^\&]+/i;
+    let untilRegex = /until=[^\&]+/i;
+    let limitRegex = /maxDataPoints=[^\&]+/i;
+
+    let query: string = this.datasource.data;
+    let replacements: [RegExp, string][] = [
+      [fromRegex, `from=${from_date}`],
+      [untilRegex, `until=${to_date}`],
+      [limitRegex, `maxDataPoints=${limit}`]
+    ];
+
+    _.each(replacements, r => {
+      let k = r[0];
+      let v = r[1];
+      if(query.search(k)) {
+        query = query.replace(k, v);
+      } else {
+        query += v;
+      }
+    });
+
     return {
-      url: `${this.datasource.url}/${q}`,
+      url: `${this.datasource.url}?${query}`,
       method: 'GET',
       schema: {
         params: this.datasource.params
@@ -55,3 +67,4 @@ export class GraphiteMetric extends AbstractMetric {
     };
   }
 }
+
