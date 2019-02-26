b1811d8bf806a8a14720473c4f90716f34bf4917
Evgeny Smyshlyaev <amper43@gmail.com>
2019-02-26 15:29:06 +0300

    GrafanaUnavailable, DatasourceUnavailable exceptions hastic/hastic-server#382 (#60)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { Metric } from './metrics/metrics_factory';
 export { Datasource } from './metrics/metric'
-export { queryByMetric, ConnectionRefused } from './grafana_service';
+export { queryByMetric, GrafanaUnavailable, DatasourceUnavailable } from './grafana_service';
