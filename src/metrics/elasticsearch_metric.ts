4f8c38ceb27ad330ea6777bb7a6d0559d2076401
Evgeny Smyshlyaev <amper43@corpglory.com>
2019-07-23 21:52:12 +0300

    Cannot set property 'gte' of undefined #80 (#82)

--- src/metrics/elasticsearch_metric.ts
+++ src/metrics/elasticsearch_metric.ts
@@ -1,7 +1,32 @@
-import { AbstractMetric, Datasource, MetricId, MetricQuery, MetricResults  } from './metric';
+import { AbstractMetric, Datasource, MetricId, MetricQuery, MetricResults } from './metric';
+import { DataKitError } from '../grafana_service';
 
 import * as _ from 'lodash';
 
+export type RangeFilter = { range: { [key: string]: { gte: String, lte: String } } };
+export type QueryStringFilter = { query_string: { analyze_wildcard: Boolean, query: String } };
+
+export type QueryConfig = {
+  size: number,
+  query: {
+    bool: {
+      filter: (RangeFilter | QueryStringFilter)[]
+    }
+  },
+  aggs: { [key: string]: Aggregation }
+};
+
+export type Aggregation = {
+  date_histogram: {
+    interval: String,
+    field: String,
+    min_doc_count: Number,
+    extended_bounds: { min: String, max: String },
+    format: String
+  }
+};
+
+const DATE_HISTOGRAM_FIELD = 'date_histogram';
 
 export class ElasticsearchMetric extends AbstractMetric {
   constructor(datasource: Datasource, targets: any[], id?: MetricId) {
@@ -10,25 +35,42 @@ export class ElasticsearchMetric extends AbstractMetric {
 
   getQuery(from: number, to: number, limit: number, offset: number): MetricQuery {
     let data = this.datasource.data.split('\n').map(d => d === '' ? d: JSON.parse(d));
-
     if(data.length === 0) {
-      throw new Error('Datasource data is empty');
+      throw new DataKitError('Datasource data is empty');
     }
 
-    data[1].size = 0;
+    const queryConfig: QueryConfig = data[1];
 
-    let filters = data[1].query.bool.filter.filter(f => _.has(f, 'range'));
+    queryConfig.size = 0;
+    let timeField = null;
+
+    let aggs = _.filter(queryConfig.aggs, f => _.has(f, DATE_HISTOGRAM_FIELD));
+    _.each(aggs, (agg: Aggregation) => {
+      agg[DATE_HISTOGRAM_FIELD].extended_bounds = {
+        min: from.toString(),
+        max: to.toString()
+      };
+
+      if(timeField !== null) {
+        console.warn(
+          `got more than one datasource time field, change ${timeField} to ${agg[DATE_HISTOGRAM_FIELD].field}`
+        );
+      }
+      timeField = agg[DATE_HISTOGRAM_FIELD].field;
+    });
+
+    if(timeField === null) {
+      throw new Error('datasource time field not found');
+    }
+
+    let filters = queryConfig.query.bool.filter.filter(f => _.has(f, 'range')) as RangeFilter[];
     if(filters.length === 0) {
-      throw new Error('Empty filters');
+      throw new DataKitError('Empty filters');
     }
     let range = filters[0].range;
-    range['@timestamp'].gte = from.toString();
-    range['@timestamp'].lte = to.toString();
-    let aggs = _.filter(data[1].aggs, f => _.has(f, 'date_histogram'));
-    _.each(aggs, agg => agg.date_histogram.extended_bounds = {
-      min: from.toString(),
-      max: to.toString()
-    });
+    range[timeField].gte = from.toString();
+    range[timeField].lte = to.toString();
+
 
     data = data
       .filter(d => d !== '')
@@ -64,7 +106,7 @@ export class ElasticsearchMetric extends AbstractMetric {
       const bucketAggs = JSON.stringify(this.targets[0].bucketAggs);
       const aggregationKeys = JSON.stringify(_.keys(aggregations));
       console.error(`can't find related aggregation id. bucketAggs:${bucketAggs} aggregationKeys:${aggregationKeys}`);
-      throw Error(`can't find related aggregation id`);
+      throw new DataKitError(`can't find related aggregation id`);
     } else {
       aggrgAgg = aggrgAgg[0].id;
     }
@@ -72,7 +114,7 @@ export class ElasticsearchMetric extends AbstractMetric {
     let agg = this.targets[0].metrics.filter(m => !m.hide).map(m => m.id);
 
     if(agg.length > 1) {
-      throw Error(`multiple series for metric are not supported currently: ${JSON.stringify(agg)}`);
+      throw new DataKitError(`multiple series for metric are not supported currently: ${JSON.stringify(agg)}`);
     }
 
     agg = agg[0];
